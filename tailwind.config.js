665dda21430470844c3351fed35ce807967658f9
arafatjamil01 <arafatjamil01@gmail.com>
2024-10-26 02:55:53 +0600

    Added links in the navigation menu

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './templates/*.liquid',
+    './templates/customers/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
